e3eb5f7bc7cfddb1ea03a8229e445874e4aa2e7f
Shaunak <shaunak@Shaunaks-MacBook-Air.local>
2022-06-04 23:52:23 +0530

    Remove hard-coded transactions

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -6,10 +6,10 @@ const GlobalState = (props) => {
   // State
   const initialState = {
     transactions: [
-      { id: 1, text: 'Flower', amount: -20 },
-      { id: 2, text: 'Salary', amount: 300 },
-      { id: 3, text: 'Book', amount: -10 },
-      { id: 4, text: 'Camera', amount: 150 },
+      //   { id: 1, text: 'Flower', amount: -20 },
+      //   { id: 2, text: 'Salary', amount: 300 },
+      //   { id: 3, text: 'Book', amount: -10 },
+      //   { id: 4, text: 'Camera', amount: 150 },
     ],
   };
 
